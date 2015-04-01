5ba1a4b57b5f681c0c18b17501527afcd712d9cf
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-01 16:01:19 -0400

    added rice to array

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,8 @@
 #----SHOPPING LIST-----
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+
+grocery_list <<"rice"
+
+grocery_list.each {|item| puts "* #{item}"}
