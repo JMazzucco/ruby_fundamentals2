9050add32ea8394f7e2eb2d5e7fd153edecf555b
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-01 22:21:58 -0400

    added method that displays array keys and values

--- exercise7.rb
+++ exercise7.rb
@@ -1,6 +1,12 @@
 students = {
-	:cohort => 34,
-	:cohort => 42,
-	:cohort => 22
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22
 }
 
+def display_cohorts(students)
+		students.each {|cohort, num| puts "#{cohort}: #{num} students"}
+end
+
+
+display_cohorts(students)
