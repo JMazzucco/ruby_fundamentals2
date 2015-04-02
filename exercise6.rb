3b6406c478fb80fe53cbab95a15f9f2b218eaf16
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-01 21:50:41 -0400

    code added: sorted array before displaying each item

--- exercise6.rb
+++ exercise6.rb
@@ -8,13 +8,11 @@ grocery_list << "rice"
 
 #method that puts each item when the grocery list array is passed to it
 def display_list(grocery_list)
-	grocery_list.each {|item| puts item}
+	grocery_list.sort.each {|item| puts item}
 	"Total Items: #{grocery_list.count}"
 end
 
-
 puts display_list(grocery_list)
-
 
 #check grocery list for bananas
 if grocery_list.include?("bananas")
@@ -23,5 +21,6 @@ else
 	puts "You don't need to pick up bananas"
 end
 
-
+#display second item in the grocery list
+puts grocery_list[1]
 
