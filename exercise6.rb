248532ccbffdb82be7e28cc2f1b85943ecad1813
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-01 21:40:07 -0400

    code added: puts array items count

--- exercise6.rb
+++ exercise6.rb
@@ -1,8 +1,22 @@
 #----SHOPPING LIST-----
 
+#initial grocery list
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+#extra item is added to the list
+grocery_list << "rice"
 
-grocery_list <<"rice"
+#method that puts each item when the grocery list array is passed to it
+def display_list(grocery_list)
+	grocery_list.each {|item| puts item}
+	"Total Items: #{grocery_list.count}"
+end
 
-grocery_list.each {|item| puts "* #{item}"}
+
+puts display_list(grocery_list)
+
+
+
+
+
+
