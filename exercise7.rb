68b4c7e5d9bfd90170f10f163dc8759879fd35ac
John Mazzucco <John.Mazzucco@gmail.com>
2015-04-01 22:26:24 -0400

    added a new key-value pair to the students hash

--- exercise7.rb
+++ exercise7.rb
@@ -4,9 +4,12 @@ students = {
 	:cohort3 => 22
 }
 
+#method formats and displays the keys and values of a hash that is passed
 def display_cohorts(students)
 		students.each {|cohort, num| puts "#{cohort}: #{num} students"}
 end
 
-
 display_cohorts(students)
+
+#add a new key-value pair to the hash
+students[:cohort4] = 43
